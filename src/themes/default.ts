2b0d268ed0775c52ba01a74c37ed778d849ec4ad
Arthur Weber <darkw@PC-Weber>
2025-09-01 15:55:40 -0300

    styled components atualizados em: Vaga, Hero e container

---
+++ src/themes/default.ts
@@ -0,0 +1,13 @@
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    corPrincipal: string
+    corSecundaria: string
+  }
+}
+
+const theme = {
+  corPrincipal: '#a7727d',
+  corSecundaria: '#f9f5e7'
+}
+
+export default theme
